7a8576bede5c5b25aa104a04fe8554879aed9fd3
Aravind Vadali <aravind.vadali@gmail.com>
2014-11-05 00:36:34 -0800

    Added guessing and losing functionality

--- Hangman.py
+++ Hangman.py
@@ -7,18 +7,59 @@ board.append("    |")
 board.append("-----")
 
 answerWord = "Jesus"
-clueWord = ""
+clueWord = []
+guessedLetters = []
 for x in range(len(answerWord)):
-	clueWord += "_ "
+	clueWord.append("_ ")
 
 def printBoard(board):
 	for row in board:
-		print " ".join(row)
+		print "".join(row)
+
+def printClueWord(clue):
+	result = ""
+	for letter in clue:
+		result += letter
+	print result
+
+def hangTheMan(board, lives):
+	#TODO:Draw the hungMan parts
+	pass
 
 def main():
-	printBoard(board)
-	print "The word is " + str(len(answerWord)) + " letters long"
-	print clueWord
+	lives = 6
+	while lives > 0:
+		printBoard(board)
+		print "The word is " + str(len(answerWord)) + " letters long"
+		printClueWord(clueWord)
+		userGuess = raw_input("Guess a letter: ")
+
+		#If user tries to guess more than a single character
+		if len(userGuess) > 1:
+			print "You may only guess one letter at a time!"
+
+		#If user guesses a letter they already guessed
+		elif userGuess in guessedLetters:
+			print "You already guessed that letter!"
+
+		#If user guesses an incorrect letter
+		elif answerWord.lower().find(userGuess.lower()) == -1:
+			print "Nope"
+			hangTheMan(board, lives)
+			lives -= 1
+
+		#If user guesses a correct letter
+		else:
+			print "Yup"
+			for index, letter in enumerate(answerWord):
+				if letter.lower() == userGuess.lower():
+					clueWord[index] = str(letter)
+
+		#Add guess to guessedLetters
+		guessedLetters.append(userGuess)
+
+	print "Game Over!\nThe word was \"" + answerWord + "\""
+			
 
 if __name__== "__main__":
 	main()
